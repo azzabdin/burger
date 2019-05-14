a3837b3c4db72e41626e8e9558b43043cd52d5ab
azza <azzabdin@gmail.com>
2019-05-14 18:27:11 -0400

    l

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,17 @@
 var mysql = require("mysql");
 
-var connection = mysql.createConnection({
+var connection ;
+if(process.env.JAWSBD_URL){
+  connection=mysql.createConnection(process.env.JAWSBD_URL);}
+   else{ connection=
+mysql.createConnection({
   host: "localhost",
   port: 3306,
   user: "root",
   password: "Anita63!",
   database: "burgers_db"
 });
-
+   };
 // Make connection.
 connection.connect(function(err) {
   if (err) {
